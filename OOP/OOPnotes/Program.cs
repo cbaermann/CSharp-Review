ee4b66eff47fee5454fe751f1a52b648085c5f91
Cody Baermann <cbaermann@comcast.net>
2019-10-10 06:22:28 -0700

    initial commit

--- OOP/OOPnotes/Program.cs
+++ OOP/OOPnotes/Program.cs
@@ -2,27 +2,67 @@
 
 namespace OOPnotes
 {
-    class Program
-    {
         class Vehicle
         {
-            private int maxNumPassengers;
-            private string color;
-            public int MaxNumPassengers{
-                get {return maxNumPassengers; }
-            }
-            public string Color{
-                get {return color; }
-            }
-            public Vehicle(int maxPass, string col)
+            public int NumPassengers;
+            public string Color;
+            protected double Odometer;
+
+            public Vehicle(int numPass, string color)
             {
-                maxNumPassengers = maxPass;
-                color = col;
+                NumPassengers = numPass;
+                Color = color;
+                Odometer = 0;
             }
+
+            public Vehicle(string color)
+            {
+                NumPassengers = 5;
+                Color = color;
+                Odometer = 0;
+            }
+
+            public virtual void GetInfo()
+            {
+                System.Console.WriteLine($"Num Passengers: {NumPassengers}");
+                System.Console.WriteLine($"Color: {Color}");
+                System.Console.WriteLine($"Miles: {Odometer}");
+            }
+        }
+
+            class Car : Vehicle
+            {
+                public string Make;
+                public string Model;
+
+                public Car(string color, string make, string model) : base(color)
+                {
+                    Make = make;
+                    Model = model;
+                }
+
+                public override void GetInfo()
+                {
+                    base.GetInfo();
+                    System.Console.WriteLine($"Make: {Make}");
+                    System.Console.WriteLine($"Model: {Model}");
+                }
+
+                public void Drive(double distance)
+                {
+                    Odometer += distance;
+                    System.Console.WriteLine("I'm Driving");
+                }
+            }
+    class Program
+    {
 
         static void Main(string[] args)
         {
-
+            Vehicle someVehicle = new Vehicle("Green");
+            Car someCar = new Car("Yellow", "Dodge", "Dart");
+            someVehicle.GetInfo();
+            someCar.GetInfo();
         }
     }
 }
